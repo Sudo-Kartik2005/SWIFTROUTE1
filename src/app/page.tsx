6fab345b3e79e3e5ce052c3104b194498118be82
Kartik <kartik829909@gmail.com>
2025-08-12 04:46:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { RideRequestForm } from '@/components/ride-request-form';
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
